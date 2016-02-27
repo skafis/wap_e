754390d18fba25d336535d463cfe7126fd622c17
FrankGwada <franciskipchumba5@gmail.com>
2016-02-27 16:57:11 +0000

    remove sql data base

--- wap_e/settings.py
+++ wap_e/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'web.apps.WebConfig',
+    # 'web',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -77,8 +79,13 @@ WSGI_APPLICATION = 'wap_e.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wap_e',
+        'USER': 'admin',
+        'PASSWORD' : 'administrator',
+        'HOST' : 'localhost',
+        'PORT': '',
+        
     }
 }
 
