5ea9733efa2c3646760abb0e206259327e4ac59c
skafis <franciskipchumba5@gmail.com>
2016-05-11 17:07:12 +0300

    define return name

--- wap_e/chillspot/models.py
+++ wap_e/chillspot/models.py
@@ -7,6 +7,11 @@ class Top_spots (models.Model):
     image = models.ImageField(upload_to='photos', verbose_name='My Photo')
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
 
 class Popular (models.Model):
     image = models.ImageField(upload_to='photos', verbose_name='My Photo')
@@ -14,9 +19,21 @@ class Popular (models.Model):
     description = models.CharField(max_length = 500)
     contact = models.CharField(max_length=10)
 
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
 class Content (models.Model):
     image = models.ImageField(upload_to='photos', verbose_name='My Photo')
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
     price = models.CharField(max_length=50)
     contact = models.CharField(max_length=10)
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
