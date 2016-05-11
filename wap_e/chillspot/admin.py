23e543e87b5b216fc1ca829cb7a9021475e26c79
skafis <franciskipchumba5@gmail.com>
2016-05-11 17:25:34 +0300

    admin working

--- wap_e/chillspot/admin.py
+++ wap_e/chillspot/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Top_spots, Popular, Content
+
+admin.site.register(Top_spots)
+admin.site.register(Popular)
+admin.site.register(Content)
--- wap_e/chillspot/models.py
+++ wap_e/chillspot/models.py
@@ -3,6 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+# class Test(models.Model):
+#     name = models.CharField(max_length=100)
+
 class Top_spots (models.Model):
     image = models.ImageField(upload_to='photos', verbose_name='My Photo')
     name = models.CharField(max_length=200)
