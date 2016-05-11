02bedb5a4efa68100d8d29a63db7200fac7c212c
skafis <franciskipchumba5@gmail.com>
2016-05-12 00:26:23 +0300

    fix static files

--- wap_e/chillspot/views.py
+++ wap_e/chillspot/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'top_spots.html')
+    return render(request, 'index.html')
